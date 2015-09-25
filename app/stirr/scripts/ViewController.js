fbdda01fd7ac2f51f8e1b53d910788ac58dbd615
Pooja Saxena <pooja335@gmail.com>
2015-09-25 14:20:08 -0500

    images working on view page

--- app/stirr/scripts/ViewController.js
+++ app/stirr/scripts/ViewController.js
@@ -7,19 +7,19 @@ angular
     $scope.dishes = [
       {
         name: 'Tomato soup',
-        icon: 'http://dingo.care2.com/pictures/greenliving/4/3519.large.jpg',
+        icon: '/tomatosoup.jpg',
         prepTime: '1 hour',
         author: 'Pooja'
       },
       {
         name: 'Ramen',
-        icon: 'http://dingo.care2.com/pictures/greenliving/4/3519.large.jpg',
+        icon: '/tomatosoup.jpg',
         prepTime: '2 hour',
         author: 'Adrian'
       },
       {
         name: 'Rice',
-        icon: 'http://dingo.care2.com/pictures/greenliving/4/3519.large.jpg',
+        icon: '/tomatosoup.jpg',
         prepTime: '1.5 hour',
         author: 'Benjamin'
       }
