ab2fbc2bd3fe8e1006a475f45389e163b98497df
Pooja Saxena <pooja335@gmail.com>
2015-09-27 12:19:35 -0500

    copied controller code from recipe to stirr

--- app/stirr/scripts/EditController.js
+++ app/stirr/scripts/EditController.js
@@ -1,14 +1,41 @@
+// angular
+//   .module('stirr')
+//   .controller('EditController', function($scope, supersonic) {
+
+//     $scope.navbarTitle = 'Edit';
+
+//     $scope.dish = {
+//                     name: 'Tomato soup',
+//                     icon: '/tomato-soup.jpg',
+//                     prepTime: '1 hour',
+//                     author: 'Pooja'
+//                   };
+
+//   });
+
 angular
-  .module('stirr')
-  .controller('EditController', function($scope, supersonic) {
+  .module('recipe')
+  .controller("EditController", function ($scope, Recipe, supersonic) {
+    $scope.recipe = null;
+    $scope.showSpinner = true;
 
-    $scope.navbarTitle = 'Edit';
+    // Fetch an object based on id from the database
+    Recipe.find(steroids.view.params.id).then( function (recipe) {
+      $scope.$apply(function() {
+        $scope.recipe = recipe;
+        $scope.showSpinner = false;
+      });
+    });
 
-    $scope.dish = {
-                    name: 'Tomato soup',
-                    icon: '/tomato-soup.jpg',
-                    prepTime: '1 hour',
-                    author: 'Pooja'
-                  };
+    $scope.submitForm = function() {
+      $scope.showSpinner = true;
+      $scope.recipe.save().then( function () {
+        supersonic.ui.modal.hide();
+      });
+    }
+
+    $scope.cancel = function () {
+      supersonic.ui.modal.hide();
+    }
 
   });
--- app/stirr/scripts/HomeController.js
+++ app/stirr/scripts/HomeController.js
@@ -1,10 +1,24 @@
+// angular
+//   .module('stirr')
+//   .controller('HomeController', function($scope, supersonic) {
+
+//     $scope.navbarTitle = 'Home';
+//     $scope.pageTitle = 'Welcome to stirr';
+
+//     $scope.teamMembers = ['Adrien', 'Benjamin', 'Kapil', 'Pooja'];
+
+//   });
+
 angular
-  .module('stirr')
-  .controller('HomeController', function($scope, supersonic) {
+  .module('recipe')
+  .controller("IndexController", function ($scope, Recipe, supersonic) {
+    $scope.recipes = null;
+    $scope.showSpinner = true;
 
-    $scope.navbarTitle = 'Home';
-    $scope.pageTitle = 'Welcome to stirr';
-
-    $scope.teamMembers = ['Adrien', 'Benjamin', 'Kapil', 'Pooja'];
-
+    Recipe.all().whenChanged( function (recipes) {
+        $scope.$apply( function () {
+          $scope.recipes = recipes;
+          $scope.showSpinner = false;
+        });
+    });
   });
