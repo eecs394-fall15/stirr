8f946ec28297a74d015abca7f6c2a3c1b1111735
Kapil Arun Garg 3 <kgarg6@sapient.com>
2015-09-27 12:40:39 -0500

    linting code

--- app/stirr/scripts/ViewController.js
+++ app/stirr/scripts/ViewController.js
@@ -29,34 +29,34 @@
 
 angular
   .module('stirr')
-  .controller('ViewController', function ($scope, Recipe, supersonic) {
+  .controller('ViewController', function($scope, Recipe, supersonic) {
     $scope.recipe = null;
     $scope.showSpinner = true;
     $scope.dataId = undefined;
 
-    var _refreshViewData = function () {
-      Recipe.find($scope.dataId).then( function (recipe) {
-        $scope.$apply( function () {
+    var _refreshViewData = function() {
+      Recipe.find($scope.dataId).then(function(recipe) {
+        $scope.$apply(function() {
           $scope.recipe = recipe;
           $scope.showSpinner = false;
         });
       });
     };
 
-    supersonic.ui.views.current.whenVisible( function () {
-      if ( $scope.dataId ) {
+    supersonic.ui.views.current.whenVisible(function() {
+      if ($scope.dataId) {
         _refreshViewData();
       }
     });
 
-    supersonic.ui.views.current.params.onValue( function (values) {
+    supersonic.ui.views.current.params.onValue(function(values) {
       $scope.dataId = values.id;
       _refreshViewData();
     });
 
-    $scope.remove = function (id) {
+    $scope.remove = function(id) {
       $scope.showSpinner = true;
-      $scope.recipe.delete().then( function () {
+      $scope.recipe.delete().then(function() {
         supersonic.ui.layers.pop();
       });
     };
