a4eddc76462384af08fffb6f09c42cd13186b2cd
Pooja Saxena <pooja335@gmail.com>
2015-09-28 02:57:11 -0500

    edit page mostly formatted

--- app/stirr/scripts/EditController.js
+++ app/stirr/scripts/EditController.js
@@ -1,18 +1,3 @@
-// angular
-//   .module('stirr')
-//   .controller('EditController', function($scope, supersonic) {
-
-//     $scope.navbarTitle = 'Edit';
-
-//     $scope.dish = {
-//                     name: 'Tomato soup',
-//                     icon: '/tomato-soup.jpg',
-//                     prepTime: '1 hour',
-//                     author: 'Pooja'
-//                   };
-
-//   });
-
 angular
   .module('stirr')
   .controller('EditController', function($scope, Recipe, supersonic) {
@@ -24,25 +9,15 @@ angular
     });
 
     var _refreshViewData = function() {
-      // for now, while we don't have a home or view page
-      var id = 'lLodrIigiW';
 
-      Recipe.find(id).then(function(recipe) {
+      // Fetch an object based on id from the database
+      Recipe.find(steroids.view.params.id).then(function(recipe) {
         $scope.$apply(function() {
           $scope.recipe = recipe;
           $scope.showSpinner = false;
         });
-        $scope.apply();
       });
     };
-
-    // // Fetch an object based on id from the database
-    // Recipe.find(steroids.view.params.id).then(function(recipe) {
-    //   $scope.$apply(function() {
-    //     $scope.recipe = recipe;
-    //     $scope.showSpinner = false;
-    //   });
-    // });
 
     $scope.submitForm = function() {
       $scope.showSpinner = true;
