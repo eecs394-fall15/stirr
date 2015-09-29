dddc2e6a72819b6d1a960ac3e2b75b256282c65d
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2015-09-28 23:38:44 -0500

    save on back

--- app/stirr/scripts/EditController.js
+++ app/stirr/scripts/EditController.js
@@ -4,6 +4,31 @@ angular
     $scope.recipe = null;
     $scope.showSpinner = true;
 
+    var _back = function() {
+      if ($scope.recipe) {
+        $scope.showSpinner = true;
+        $scope.recipe.save().then(function() {
+          $scope.showSpinner = false;
+          supersonic.ui.layers.pop();
+        });
+      }
+    };
+
+    var _backButton = new supersonic.ui.NavigationBarButton({
+      title: 'Back',
+      onTap: _back
+    });
+
+    var _options = {
+      title: 'stirr',
+      overrideBackButton: true,
+      buttons: {
+        left: [_backButton]
+      }
+    };
+
+    supersonic.ui.navigationBar.update(_options);
+
     // Fetch an object based on id from the database
     Recipe.find(steroids.view.params.id).then(function(recipe) {
       $scope.$apply(function() {
@@ -11,16 +36,5 @@ angular
         $scope.showSpinner = false;
       });
     });
-
-    $scope.submitForm = function() {
-      $scope.showSpinner = true;
-      $scope.recipe.save().then(function() {
-        supersonic.ui.modal.hide();
-      });
-    };
-
-    $scope.cancel = function() {
-      supersonic.ui.modal.hide();
-    };
 
   });
