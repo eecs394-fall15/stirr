683b1b70a7a125f32f5e6b1bdd3a62961bc3d4e2
Kapil Arun Garg 3 <kgarg6@sapient.com>
2015-09-28 23:20:09 -0500

    minor fix

--- app/stirr/scripts/EditController.js
+++ app/stirr/scripts/EditController.js
@@ -3,13 +3,13 @@ angular
   .controller('EditController', function($scope, Recipe, supersonic) {
     $scope.recipe = null;
     $scope.showSpinner = true;
+    $scope.hello = "hello";
 
     supersonic.ui.views.current.whenVisible(function() {
       _refreshViewData();
     });
 
     var _refreshViewData = function() {
-
       // Fetch an object based on id from the database
       Recipe.find(steroids.view.params.id).then(function(recipe) {
         $scope.$apply(function() {
