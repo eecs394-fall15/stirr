7d98cc68d3d1c17ac4881b1e3e1bafe642a07c4f
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2015-09-28 21:56:15 -0500

    simpler solution?

--- app/stirr/scripts/EditController.js
+++ app/stirr/scripts/EditController.js
@@ -4,32 +4,19 @@ angular
     $scope.recipe = null;
     $scope.showSpinner = true;
 
-    supersonic.ui.views.current.whenVisible(function() {
-      _refreshViewData();
-    });
-
-    var _refreshViewData = function() {
-
-      // Fetch an object based on id from the database
-      Recipe.find(steroids.view.params.id).then(function(recipe) {
-        $scope.$apply(function() {
-          $scope.recipe = recipe;
-          $scope.showSpinner = false;
-        });
+    // Fetch an object based on id from the database
+    Recipe.find(steroids.view.params.id).then(function(recipe) {
+      $scope.$apply(function() {
+        $scope.recipe = recipe;
+        $scope.showSpinner = false;
       });
-    };
-
-    _refreshViewData();
+    });
 
     $scope.submitForm = function() {
       $scope.showSpinner = true;
       $scope.recipe.save().then(function() {
         supersonic.ui.modal.hide();
       });
-
-      // update recipe page
-      _refreshViewData();
-
     };
 
     $scope.cancel = function() {
