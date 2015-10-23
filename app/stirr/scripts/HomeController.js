b2fe14c5b05676f48008995b2ba037a5225f84f6
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2015-10-22 21:35:47 -0500

    removed parentId from schema

--- app/stirr/scripts/HomeController.js
+++ app/stirr/scripts/HomeController.js
@@ -23,7 +23,7 @@ angular
     var _getRecipes = function() {
       $scope.showSpinner = true;
       _whenDeviceReady(function() {
-        var unsubscribe = Recipe.all().whenChanged(function(recipes) {
+        Recipe.findAll().then(function(recipes) {
           $scope.$apply(function() {
             if ($scope.filtering) {
               $scope.recipes = recipes.filter(function(recipe) {
@@ -34,8 +34,7 @@ angular
             }
             $scope.showSpinner = false;
           });
-          unsubscribe();
-        });
+        }, function(err) {globalError = err;});
       });
     };
 
