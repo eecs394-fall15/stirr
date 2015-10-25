17b5bd115058fe29789cd3354ada1e1bb01dc300
Kapil Arun Garg 3 <kgarg6@sapient.com>
2015-10-25 18:41:25 -0500

    code cleanup.

--- app/stirr/scripts/VersionViewController.js
+++ app/stirr/scripts/VersionViewController.js
@@ -4,32 +4,15 @@ angular
 
     $scope.recipes = null;
     $scope.showSpinner = true;
-    $scope.filtering = true;
-
-    var deviceReady = false;
-
-    var _whenDeviceReady = function(callback) {
-      if (deviceReady) {
-        callback();
-      } else {
-        angular.element(document).on('deviceready', callback);
-      }
-    };
-
-    angular.element(document).on('deviceready', function() {
-      deviceReady = true;
-    });
 
     var _getRecipes = function() {
       $scope.showSpinner = true;
-      _whenDeviceReady(function() {
-        Recipe.findAll().then(function(recipes) {
-          $scope.$apply(function() {
-            $scope.recipes = recipes.filter(function(recipe) {
-              return recipe.parentId === steroids.view.params.parentId;
-            });
-            $scope.showSpinner = false;
+      Recipe.findAll().then(function(recipes) {
+        $scope.$apply(function() {
+          $scope.recipes = recipes.filter(function(recipe) {
+            return recipe.parentId === steroids.view.params.parentId;
           });
+          $scope.showSpinner = false;
         });
       });
     };
